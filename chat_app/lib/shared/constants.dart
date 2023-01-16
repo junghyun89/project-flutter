a0faceef5a4530d05abd866678741467887cd1db
jung89 <wjdgus8970@gmail.com>
2023-01-16 23:59:52 +0900

    ing

---
+++ chat_app/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+
+class Constants {
+  static String appId = "1:673799130117:web:a94b95c9bab559f216206c";
+  static String apiKey = "AIzaSyDR7BuAlMfb0d-0YIshsnImnH2tJ_SAXEk";
+  static String messagingSenderId = "673799130117";
+  static String projectId = "chatappflutter-a681d";
+  final primaryColor = const Color(0xFFee7b64);
+}
